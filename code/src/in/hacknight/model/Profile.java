5903671aebb8de8a85695c4fb535afd86ad31745
garima sharma <sharma_garima27@yahoo.com>
2013-11-30 02:11:16 +0530

    garima | added model

---
+++ code/src/in/hacknight/model/Profile.java
@@ -0,0 +1,15 @@
+package in.hacknight.model;
+
+public class Profile {
+    public String name = null;
+    public int property = 0;
+    public int duration = 0;
+
+
+    public Profile(String name, int property, int duration) {
+        this.name = name;
+        this.property = property;
+        this.duration = duration;
+    }
+
+}
